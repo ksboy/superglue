a22bb7ec1805cc54d82039ae2e8e37a214b1119e
ksboy <weihouks@gmail.com>
2019-07-25 09:53:23 +0000

    unknown

--- cb.sh
+++ cb.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 CUDA_VISIBLE_DEVICES=2,3 python run_classifier.py  \
        --data_dir=../data-superglue/CB  \
-       --bert_model=../bert-large-cased-wwm-mnli/ \
+       --bert_model=../xlnet-large-cased \
        --task_name=cb \
-       --output_dir=./outputs/cb10_remain_loss/ \
+       --output_dir=./outputs/cb20/ \
        --cache_dir=./cache/  \
        --do_train \
-       --num_train_epochs=10 \
+       --num_train_epochs=3 \
        --learning_rate=1e-5 \
        --train_batch_size=32 \
        --do_eval \
-       --loss_weight=1,1,5
+      #  --loss_weight=1,1,5
     #    --pop_classifier_layer  
 
