7b9c0fba8684722f505c023993ed8549d453ccac
ksboy <weihouks@gmail.com>
2019-08-09 02:11:00 +0000

    test seed

--- copa.sh
+++ copa.sh
@@ -1,33 +1,33 @@
 #!/usr/bin/env bash
-# train_size =400  sum_batch_size = 16 steps_per_epoch =25  sum_steps= 480
+# train_size =400  sum_batch_size = 16 steps_per_epoch =25  sum_steps= 250
 
-
-CUDA_VISIBLE_DEVICES=0,2,3 python run_superglue.py \
+for SEED in 3 7 42 50 87
+do 
+CUDA_VISIBLE_DEVICES=2,3 python run_superglue.py \
     --model_type=xlnet \
     --model_name_or_path=./outputs/xlnet-large-cased-swag/checkpoint-27000 \
     --do_train  \
     --do_eval   \
-    --save_steps=80 \
+    --save_steps=-1 \
     --eval_all_checkpoints \
     --logging_steps=10 \
     --evaluate_during_training  \
-    --task_name=cb  \
+    --task_name=copa  \
     --data_dir=../data-superglue/COPA \
-    --output_dir=./outputs/copa25   \
+    --output_dir=./outputs/copa_bert/8/$SEED   \
     --cache_dir=./cache \
     --max_seq_length=128   \
     --per_gpu_eval_batch_size=8   \
     --per_gpu_train_batch_size=8   \
     --learning_rate=1e-5 \
-    --num_train_epochs=30.0
-    # --max_steps= 
+    --num_train_epochs=10.0 \
+    --max_steps=250 \
+    --warmup_steps=15 \
+    --pop_layer=logits
     # --tokenizer_name=xlnet_large_cased  \
     # --gradient_accumulation_steps=1 \
-    # --max_steps=1200  \
-    # --model_name=xlnet-large-cased   \
-    # --warmup_steps=120 \
     # --do_lower_case \
     # --overwrite_output_dir   \
     # --overwrite_cache \
-    --pop_classifier_layer  
 
+done
