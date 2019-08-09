0d89f07c4be1413e167fb4afa6ab1869b7e90376
ksboy <weihouks@gmail.com>
2019-08-09 06:38:30 +0000

    update

--- copa.sh
+++ copa.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# train_size =400  sum_batch_size = 16 steps_per_epoch =25  sum_steps= 250
+# train_size =400*2  sum_batch_size = 16*2 steps_per_epoch =25  sum_steps= 250
 
 for SEED in 3 7 42 50 87
 do 
@@ -14,16 +14,17 @@ CUDA_VISIBLE_DEVICES=2,3 python run_superglue.py \
     --evaluate_during_training  \
     --task_name=copa  \
     --data_dir=../data-superglue/COPA \
-    --output_dir=./outputs/copa_bert/8/$SEED   \
+    --output_dir=./outputs/copa_bert/$SEED   \
     --cache_dir=./cache \
     --max_seq_length=128   \
-    --per_gpu_eval_batch_size=8   \
-    --per_gpu_train_batch_size=8   \
+    --per_gpu_eval_batch_size=16   \
+    --per_gpu_train_batch_size=16   \
     --learning_rate=1e-5 \
     --num_train_epochs=10.0 \
     --max_steps=250 \
     --warmup_steps=15 \
-    --pop_layer=logits
+    --pop_layer=logits \
+    --seed=$SEED \
     # --tokenizer_name=xlnet_large_cased  \
     # --gradient_accumulation_steps=1 \
     # --do_lower_case \
